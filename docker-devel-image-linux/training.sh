2404682aa5207b50d6ce4c665f7579b4a8392d97
Lara CODECA <lara.codeca@gmail.com>
2020-10-22 11:56:40 +0100

    RLLIB integration test and devel docker #11
    See: https://github.com/lcodeca/ray/tree/sumo-gym-env

---
+++ docker-devel-image-linux/training.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Example of learning script for SUMO Utlis RLLIB integration
+#
+# Author: Lara CODECA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+
+set -e
+
+# SUMO-dev environmental vars
+export SUMO_HOME="/home/alice/sumo"
+export SUMO_BIN="$SUMO_HOME/bin"
+export SUMO_TOOLS="$SUMO_HOME/tools"
+export PATH="$SUMO_BIN:$PATH"
+
+echo "Testing SUMO command"
+sumo --version
+
+echo "Testing Tensorflow resources"
+python tf-gpu-test.py
+
+echo "Testing RLLIB SUMO Utils example"
+python /home/alice/libraries/ray/rllib/examples/sumo_env_local.py --as-test
---
+++ rllib-devel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Example of docker launcher SUMO Utlis RLLIB integration
+#
+# Author: Lara CODECA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+
+# echo "Parameters:"
+# echo "  IMAGE name \"$IMAGE_NAME\" [-n, --image-name]"
+# echo "  IMAGE folder \"$IMAGE_FOLDER\" [-f, --image-folder]"
+# echo "  GPU enabled ($GPU) [--no-gpu]"
+# echo "  OPTIRUN disabled ($OPTIRUN) [--with-optirun]"
+# echo "  BUILD: $BUILD [-b, --build], with CACHE: $CACHE [-c, --cache]"
+# echo "  RUN: $RUN [-r, --run], with SCREEN: $SCREEN [-s, --screen]"
+# echo "  EXEC: $EXEC [-e, --exec], CONTAINER: \"$CONTAINER\" (use docker ps for the id)"
+# echo "  COMMAND: \"$COMMAND\" [--cmd]"
+# echo "  EXP: \"$EXP\" [--exp]"
+# echo "  DETACH: ($DETACH) [--detach]"
+# echo "  DEVELOPMENT dir \"$DEVEL_DIR\" [-d, --devel]"
+# echo "  LEARNING dir \"$LEARN_DIR\" [-l, --learn]"
+# echo "  SHM_SIZE \"$SHM_SIZE\" [--shm-size]"
+
+bash docker-cmd-linux.sh -n="rllib-devel" -f="docker-devel-image-linux" \
+    --with-optirun -b -c -r --exp="rllib-sumo-devel" --cmd="/bin/bash"
